4b68e6d60c005bb037984fae84a4f31aacdf6d97
unknown <>
2018-04-04 12:04:48 +0800

    后台加了个可以加个栏目的属性

--- application/api/controller/Promote.php
+++ application/api/controller/Promote.php
@@ -6,14 +6,25 @@ class Promote extends \think\Controller
 {
 	function index()
 	{
-		 
+		 //多表连接
 			$list = db("smalltalk")
-				->order('id desc')
+				->alias('s')
+				->join('vip v','v.id = s.vip_id')
+				->join('vip_cate vc','v.vip_cate_id = vc.id')
+				->order('s.id desc')
 				->paginate(3);
-		 
+		 		
 		return json($list);
 	}
-	 
+	 // public function vip()
+	 // {
+	 // 	$info=db('vip')
+	 // 	->alias('v')
+	 // 	->join('vip_cate vc','v.vip_cate_id = vc.id')
+	 // 	->select();
+	 // 	return json($info);
+
+	 // }
 	
 }
 
