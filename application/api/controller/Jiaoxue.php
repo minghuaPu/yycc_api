ec6454e00a68d3488ddcd08965b38ebcec0bf9f2
zhujinyi <1309910059@qq.com>
2018-04-23 17:16:38 +0800

    教学修改2

--- application/api/controller/Jiaoxue.php
+++ application/api/controller/Jiaoxue.php
@@ -28,9 +28,33 @@ class Jiaoxue extends \think\Controller
 					->alias('jx_xq')
 					->field('jx_xq.*,u.user_name,u.head_img')
 					->join('user u','u.id = jx_xq.uid')
-					->order('jx_xq.addtime desc')
+					// ->where('jx_xq.status = 1')
+					->order('jx_xq.money desc')
 					->paginate(5,false,['page'=>$page]);
 		return json($xuqiu_lists);
+	}
+	public function teach()
+	{
+		$has_jiao = db('jiaoxue_xuqiu')->where('id = '.input('xq_id'))->find();
+		// print_r($has_jiao);
+		// echo($has_jiao['teacher_id']);
+
+		// exit();
+		if(empty($has_jiao['teacher_id'])){
+			db('jiaoxue_xuqiu')->where('id = '.input('xq_id'))->update([
+				'status'=>2,
+				'teacher_id'=>input('teach_id')
+			]);
+			return json([
+				'status'=>1,
+				'msg'=>'开始教学'
+			]);
+		}else{
+			return json([
+				'status'=>2,
+				'msg'=>'手速慢了，已有被教！'
+			]);
+		}
 	}
 	
 }
