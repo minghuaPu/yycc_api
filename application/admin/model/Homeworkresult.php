2f98c014c760cd49011980e1413807f81b42b94d
xzc1366 <Mr_xian12@163.com>
2018-06-01 19:41:52 +0800

    提交作业管理做按班级查询功能

--- application/admin/model/Homeworkresult.php
+++ application/admin/model/Homeworkresult.php
@@ -39,6 +39,56 @@ class Homeworkresult extends \think\Model{
        		return db('homework_result')->where('id='.input('id'))->find();
 
     }
+    public function getHomeworkResult(){
+    	//查询班级信息
+        $bj=db('banji')->select();
+        //按班级编号查询
+        if(input('bj_id')){
+            //print_r(input('bj_id'));exit();
+            $banji=input('bj_id');
+            $pageParam = ['query'=>[]];
+            $pageParam['query']['bj_id']=$banji;
+           // $homework_result_list = db('homework_result')->where('u_id','in', $uid)->order('id desc')->paginate(20);
+            $homework_result_list = db('homework_result')
+					->alias('v')
+					->join('homework_rel vc','vc.id=v.hw_id')
+					->field('v.*')
+					->order('v.id desc')
+					->where("vc.banji=$banji")
+					->paginate(2,false,$pageParam);
+        }//无条件查询
+        else{
+    	    $homework_result_list= db('homework_result')
+					->alias('v')
+					->join('homework_rel vc','vc.id=v.hw_id')
+					->field('v.*')
+					->order('v.id desc')
+					->paginate(10,false);
+        }
+        $hao= array( );
+        foreach($homework_result_list as $key=>$value){    
+        if($value['hao_time']>60){
+            $haotime=$value['hao_time']/60;
+            if(is_int($haotime)){
+                $hao[]=$haotime."分"; 
+                  
+            }else{
+               $mo=$value['hao_time']%60;
+               $hao[]=intval($haotime)."分".$mo."秒";
+               
+            }
+        }else{
+              $hao[]=$value['hao_time']."秒";
+              
+        }  
+
+        }
+        $hrl['bj']=$bj;
+        $hrl['hrl']=$homework_result_list;
+        $hrl['hao']=$hao;
+		return $hrl ;
+
+    }
 
 }
 
