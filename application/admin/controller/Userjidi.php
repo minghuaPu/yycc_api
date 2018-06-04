34b4735beac3eeceb3a111a312ffb9e328a68948
slzhang1991 <208653385@qq.com>
2018-06-04 09:13:16 +0800

    添加订单列表导出功能

--- application/admin/controller/Userjidi.php
+++ application/admin/controller/Userjidi.php
@@ -2,12 +2,18 @@
 namespace app\admin\controller;
 
 class Userjidi extends \app\admin\controller\Auth {
+	// *********************** 报名管理模块 *******************************
+
 	// 学生列表
 	public function index() {
-		$apply_list = model("Userjidi")->getApplyList();
-		$this->assign("apply_list", $apply_list);
+		// 获取学生列表
+		$stu_list = model("Userjidi")->getStuList();
+		$this->assign("stu_list", $stu_list);
+		// 回填查询信息
+		$this->assign("uname", input('uname'));
+		$this->assign("school_name", input('school_name'));
+		// 返回开通状态列表
 		$this->assign("pay_status", user_kaitong_status());
-		
 		return $this->fetch();
 	}
 
@@ -62,14 +68,68 @@ class Userjidi extends \app\admin\controller\Auth {
 		$this->success("更新成功","index", '', 1);
 	}
 
+	// *********************** 订单管理模块 *******************************
+
 	// 订单管理
 	public function orderManager() {
 		$orders = model('Userjidi')->orderManager();
-		$this->assign('pay_status', user_kaitong_status());
-		$this->assign('order_type', orders_type());
 		$this->assign('orders', $orders);
-		// print_r($orders);
+		// 返回开通状态数组
+		$this->assign('pay_status', user_kaitong_status());
+		// 返回订单类型数组
+		$this->assign('orders_type', orders_type());
+		// 数据回显
+		$this->assign('uname', input('uname'));
+		$this->assign('order_type', input('order_type'));
 		return $this->fetch();
+	}
+
+	// 导出订单列表
+	public function exportExcel() {
+		require(ROOT_PATH)."/extend/PHPExcel/PHPExcel.php";
+		require(ROOT_PATH)."/extend/PHPExcel/PHPExcel/Worksheet/ColumnDimension.php";
+		$excelObj = new \PHPExcel();
+		$sheetObj = $excelObj->getActiveSheet();
+		$sheetObj->getRowDimension(1)->setRowHeight(30);
+		$sheetObj->setTitle("订单列表");
+		$sheetObj->getColumnDimension('A')->setWidth(16); 
+		$sheetObj->getColumnDimension('B')->setWidth(25); 
+		$sheetObj->getColumnDimension('C')->setWidth(16); 
+		$sheetObj->getColumnDimension('D')->setWidth(16); 
+		$sheetObj->getColumnDimension('E')->setWidth(16); 
+		$sheetObj->mergeCells('A1:E1')->setCellValue('A1', '订单列表');
+		$sheetObj->getDefaultStyle()->getAlignment()->setVertical(\PHPExcel_Style_Alignment::VERTICAL_CENTER)->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+		$sheetObj->getStyle('A1:E1')->getFont()->setName("微软雅黑")->setSize(18)->setBold(true);
+		$sheetObj->getStyle('A2:E2')->getFont()->setName("微软雅黑")->setSize(12)->setBold(true);
+		// 设置标题
+		$sheetObj->setCellValue('A2', '用户名')->setCellValue('B2', '订单编号')->setCellValue('C2', '订单日期')->setCellValue('D2', '订单类型')->setCellValue('E2', '支付状态');
+		// 获取数据
+		$orders = model('Userjidi')->orderList();
+		// print_r($orders);
+		$arr = user_kaitong_status();
+		$type = orders_type();
+		// 遍历并向excel文件中写入数据
+		foreach ($orders as $key => $order) {
+			$sheetObj->setCellValue('A'.($key + 3), $order['user_name'])->setCellValue('B'.($key + 3), ' '.$order['out_trade_no'].' ')->setCellValue('C'.($key + 3), date("Y-m-d", $order['add_time']))->setCellValue('D'.($key + 3), $type[$order['order_type']])->setCellValue('E'.($key + 3), $arr[$order['is_pay']]);
+		}
+		$objWriter = \PHPExcel_IOFactory::createWriter($excelObj, 'Excel2007');
+		$this->exportToBrowser("Excel2007", "订单列表.xlsx");
+		$objWriter->save('php://output');
+	}
+
+	// 向浏览器输出excel文件
+	public function exportToBrowser($file_type, $file_name) {
+		// 清除缓冲区
+		ob_end_clean();
+		// 判断Office版本
+		if ($file_type == "Excel5") {
+			header('Content-Type: application/vnd.ms-excel');
+		} else {
+			header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		}
+		header('Content-Disposition: attachment;filename="'.$file_name.'"');
+		// 禁止缓存
+		header('Cache-Control: max-age=0');
 	}
 
 }
